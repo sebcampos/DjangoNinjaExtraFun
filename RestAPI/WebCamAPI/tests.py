3e4e155d308fb4cdd2f8aa691b540d398eb43844
sebash <sebcampos@criptext.com>
2024-03-24 20:52:34 -0700

    adding django channels

--- RestAPI/WebCamAPI/tests.py
+++ RestAPI/WebCamAPI/tests.py
@@ -8,11 +8,10 @@ PORT = 8080
 
 # Create your tests here.
 def test_local_camera_stream():
-    count = 0
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         s.sendall(b'connect')
-        while count < 100:
+        while True:
             size_m = s.recv(1024)
             size = int(size_m.split()[-1])
             s.sendall(b'received frame size')
@@ -22,13 +21,12 @@ def test_local_camera_stream():
             image = cv2.imdecode(np.asarray(bytearray(frame)), -1)
             image = cv2.resize(image, (5000, 5000))
             cv2.imshow("image", image)
-            count += 1
-            if count != 100:
-                s.sendall(b'received frame')
-        s.sendall(b'close')
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                s.sendall(b"close")
+                break
+            s.sendall(b'received frame')
         cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
     test_local_camera_stream()
-"incremental, txaio, setuptools, pycparser, pyasn1, idna, constantly, attrs, zope-interface, pyasn1-modules, hyperlink, channels, cffi, automat, twisted, cryptography, service-identity, pyopenssl, autobahn, daphne"
