179f6188387f6a9151a5c015662b5b81683d6105
sebash <sebcampos@criptext.com>
2024-03-24 20:40:57 -0700

    adding depencies for django channels

--- RestAPI/RestAPI/api.py
+++ RestAPI/RestAPI/api.py
@@ -1,5 +1,19 @@
 from ninja_extra import NinjaExtraAPI
 from WebCamAPI.api_contoller import CameraController
+from ninja_extra import NinjaExtraAPI
+from ninja.security import APIKeyHeader
+from ninja.security import django_auth
 
-api = NinjaExtraAPI(title="WebCamAPI", version="0.0.1")
+
+class ApiKey(APIKeyHeader):
+    param_name = "X-API-Key"
+
+    def authenticate(self, request, key):
+        if key == "supersecret":
+            return key
+
+
+header_key = ApiKey()
+
+api = NinjaExtraAPI(title="WebCamAPI", version="0.0.1", csrf=True, auth=[header_key, django_auth])
 api.register_controllers(CameraController)
--- RestAPI/WebCamAPI/tests.py
+++ RestAPI/WebCamAPI/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+import socket, cv2
+import numpy as np
+
+HOST = "127.0.0.1"
+PORT = 8080
+
 
 # Create your tests here.
+def test_local_camera_stream():
+    count = 0
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b'connect')
+        while count < 100:
+            size_m = s.recv(1024)
+            size = int(size_m.split()[-1])
+            s.sendall(b'received frame size')
+            frame = b""
+            while len(frame) < size:
+                frame += s.recv(size)
+            image = cv2.imdecode(np.asarray(bytearray(frame)), -1)
+            image = cv2.resize(image, (5000, 5000))
+            cv2.imshow("image", image)
+            count += 1
+            if count != 100:
+                s.sendall(b'received frame')
+        s.sendall(b'close')
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    test_local_camera_stream()
+"incremental, txaio, setuptools, pycparser, pyasn1, idna, constantly, attrs, zope-interface, pyasn1-modules, hyperlink, channels, cffi, automat, twisted, cryptography, service-identity, pyopenssl, autobahn, daphne"
