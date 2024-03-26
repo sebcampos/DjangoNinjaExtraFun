37dcd6b9fc9e4d859dbd388fc4bc7e1214e85928
Sebastian Campos <sebash@macbook-pro.lan>
2024-03-25 21:29:36 -0700

    working on channels

--- RestAPI/RestAPI/asgi.py
+++ RestAPI/RestAPI/asgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from . import routing
+from WebCamAPI.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RestAPI.settings')
 
@@ -20,7 +20,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            routing.websocket_urlpatterns
+            websocket_urlpatterns
         )
     ),
 })
