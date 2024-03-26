e9cd1964650058b14240771e2c7534fe10aabf80
Sebastian Campos <sebash@macbook-pro.lan>
2024-03-25 23:21:58 -0700

    adding to allowed hosts

--- RestAPI/WebCamAPI/consumers.py
+++ RestAPI/WebCamAPI/consumers.py
@@ -2,17 +2,17 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 
 class VideoConsumer(WebsocketConsumer):
-    def connect(self):
+    async def connect(self):
         print('connected')
         # connect to the video stream
         self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # disconnect from the video stream
         print('disconnected')
         pass
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         print("received text: " + text_data)
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
