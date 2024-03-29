70f9e6b190fa487159b3d96e495b6d909dbfa44e
Sebastian Campos <sebcampos@criptext.com>
2024-03-29 12:16:22 -0700

    chat server working

--- RestAPI/WebCamAPI/consumers.py
+++ RestAPI/WebCamAPI/consumers.py
@@ -4,23 +4,34 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class VideoConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print('connected')
-        # connect to the video stream
-        await self.channel_layer.group_add("cam", "live")
+        # connection from client add to cam `group`
+        await self.channel_layer.group_add("cam", self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        # disconnect from the video stream
-        print('disconnected')
-
-        await self.channel_layer.group_discard("cam", "live")
+        # disconnect from the video stream, remove from cam group
+        await self.channel_layer.group_discard("cam", self.channel_name)
 
     async def receive(self, text_data):
+        # receive a message from cam
         print("received text: " + text_data)
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        message = text_data_json["message"]
 
         # Broadcast the received message to all clients
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        await self.channel_layer.group_send(
+            "cam",
+            {
+                "type": "chat.message",
+                "message": message
+            }
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        # this method maps to the chat.message type, invoked by broadcasted channel_layer.groupsend
+        message = event["message"]
+
+        print("CHATMESSAGE")
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message}))
