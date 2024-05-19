e3717c0a7fc02eb8f4a144337dd1b64e88def566
sebastian campos <sebcampos@criptext.com>
2024-05-19 16:15:34 -0700

    working on video recording portion

---
+++ RestAPI/shutdown_camera.py
@@ -0,0 +1,8 @@
+HOST = "127.0.0.1"
+PORT = 8080
+def shutdown_cam_feed_server():
+    import socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b'shutDown')
+
